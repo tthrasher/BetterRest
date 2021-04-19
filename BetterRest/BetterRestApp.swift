a7ebeaba8e6a911707da093948697a46075c609a
tthrasher <tthrasher@gmail.com>
2021-04-19 15:54:05 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Terry Thrasher on 2021-04-19.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
